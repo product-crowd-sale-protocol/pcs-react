25fa18c0783e2732fe3b9dee7824ef475b353cd4
Akatsuki-py <bluejapan73+dev@gmail.com>
2019-06-18 21:45:43 +0900

    export networkList in main.js

--- src/main.js
+++ src/main.js
@@ -5,3 +5,5 @@ export Dex from "./Dex/Dex";
 export DexBoard from "./Dex/Board/DexBoard";
 export Chart from "./Dex/Chart/Chart";
 export DexForm from "./Dex/Form/DexForm";
+import { PcsClient } from "pcs-js-eos";
+export const networkList = PcsClient.networkList;
